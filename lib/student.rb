a9e4c1b0bbba614b0ec78244bc8ffe056c0df2b2
JStern95 <JSternberg95@gmail.com>
2018-09-06 11:47:03 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,42 +1,48 @@
+require "pry"
 class Student
+
   attr_accessor :name, :grade
   attr_reader :id
 
   def initialize(name, grade, id=nil)
-    @id = id
     @name = name
     @grade = grade
+    @id = id
   end
 
   def self.create_table
-    sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
-        )
-    SQL
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER);
+      SQL
     DB[:conn].execute(sql)
   end
 
   def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
+    sql= <<-SQL
+    DROP TABLE students;
+    SQL
     DB[:conn].execute(sql)
   end
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
     SQL
-
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0].first
   end
 
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
+  def self.create(hash={})
+    #binding.pry
+      student = Student.new(hash[:name], hash[:grade])
     student.save
     student
   end
+
+
+
 end
